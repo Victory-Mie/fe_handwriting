37fe57677130cc642fcded62813f20e0d38aa3a3
CathyWong <1064348766@qq.com>
2025-04-11 14:53:41 +0800

    fix

--- js/07.字符串/04.解析URL参数.js
+++ js/07.字符串/04.解析URL参数.js
@@ -15,3 +15,20 @@ function parseUrlParams(url) {
 const url = "https://example.com?name=John&age=30";
 const paramsObject = parseUrlParams(url);
 console.log(paramsObject);
+
+// 方法二
+const getUrlParams = (url) => {
+  const arrSearch = url.split("?").pop().split("#").shift().split("&");
+  let obj = {};
+  arrSearch.forEach((item) => {
+    const [k, v] = item.split("=");
+    obj[k] = v;
+    return obj;
+  });
+  return obj;
+};
+
+// test
+const url2 = "http://sample.com/?a=1&b=2&c=xx&d=2#hash";
+const paramsObject2 = getUrlParams(url2);
+console.log(paramsObject2);
